e9f11be3241cfb3d952deb7febc0d62adc4b2de8
ByungyeonKim <viva_kby@naver.com>
2020-07-19 21:14:32 +0900

    CSS: body
    React.js - JS, CSS 환경구축

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className='App'> Hello, React!!</div>;
+    return (
+      <div className='App'>
+        Hello, React!! <br />
+        리액트 JS,CSS 환경 구축 성공!
+      </div>
+    );
   }
 }
 
