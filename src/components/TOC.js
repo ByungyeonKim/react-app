25b4141bd4fe13f41a57fb5d28aa4f78b661938a
ByungyeonKim <viva_kby@naver.com>
2020-09-01 23:27:49 +0900

    shouldComponentUpdate

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
+  // 현재 데이터와 새로운 데이터가 같다면 render함수를 호출하지 않는다.(성능개선)
+  // https://ko.reactjs.org/docs/react-component.html#shouldcomponentupdate
+  // 기본 동작은 매 state 변화마다 다시 렌더링을 수행하는 것이며, 대부분의 경우 기본 동작에 따라야 한다.
+  // props 또는 state가 새로운 값으로 갱신되어서 렌더링이 발생하기 직전에 호출된다.
+  shouldComponentUpdate(newProps) {
+    if (this.props.data === newProps.data) {
+      return false;
+    }
+    return true;
+  }
   render() {
     let lists = [];
     let data = this.props.data;
